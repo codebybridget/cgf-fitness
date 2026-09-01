2bad669648bdaf2e79c873c33c46769e974e19ae
codebybridget <adeyeyebridget9@gmail.com>
2026-09-01 08:45:47 -0700

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -8,7 +8,7 @@ import axios from "axios"
 
 const API_BASE_URL =
   import.meta.env.VITE_API_URL ||
-    "https://cgf-fitness.onrender.com/api"
+   "http://localhost:5000/api"
 
 /*
 |--------------------------------------------------------------------------
